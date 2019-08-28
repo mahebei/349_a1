858ea8bfcd2d529e6d47341bdc16179d85a525a1
mahebei <31981108+mahebei@users.noreply.github.com>
2019-08-29 09:35:59 +1200

    Add two attributes: default from and to cities

--- setup-user-database.sql
+++ setup-user-database.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
   uname varchar(10),
+  fromcity varchar(80) not null,
+  tocity varchar(80) not null,
   PRIMARY KEY (uname)
 );
 
-INSERT INTO users VALUES ('user1');
-INSERT INTO users VALUES ('user2');
+INSERT INTO users VALUES ('admin', 'Auckland, Wellington', 'GMT: Dublin, Edinburgh, Lisbon, London');
+INSERT INTO users VALUES ('user', 'Alaska', 'Hawaii');
